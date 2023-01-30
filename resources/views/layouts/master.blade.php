fcc6549c26e385b6593faf2e6b307e59b6f2fc5e
SS59lp3i <ss59lp3i@gmail.com>
2023-01-30 09:59:45 +0700

    penyesuaian master blade

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Blank Page</title>
+  <title>@yield('judul')</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -227,7 +227,7 @@
             </ul>
           </li>
           <li class="nav-item">
-            <a href="../widgets.html" class="nav-link">
+            <a href="/mahasiswa" class="nav-link">
               <i class="nav-icon fas fa-users"></i>
               <p>
                 Data Mahasiswa
@@ -235,7 +235,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="../widgets.html" class="nav-link">
+            <a href="/jabatan" class="nav-link">
               <i class="nav-icon fas fa-user-tie"></i>
               <p>
                 Data Jurusan
@@ -253,7 +253,7 @@
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <section class="content-header">
+    {{-- <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
@@ -267,10 +267,10 @@
           </div>
         </div>
       </div><!-- /.container-fluid -->
-    </section>
-
+    </section> --}}
+    @yield('conten-header')
     <!-- Main content -->
-    <section class="content">
+    {{-- <section class="content">
 
       <!-- Default box -->
       <div class="card">
@@ -297,7 +297,8 @@
       </div>
       <!-- /.card -->
 
-    </section>
+    </section> --}}
+    @yield('content')
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
